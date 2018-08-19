42f8a9afc6a8901471326a374a123a5c98517f31
Darío Alonso <dariovillar@hotmail.com>
2018-08-19 23:25:16 +0200

    using params to add content and removing intilization

--- demo/FluentDemo/FluentDemo/MainView.cs
+++ demo/FluentDemo/FluentDemo/MainView.cs
@@ -1,34 +1,19 @@
 ﻿using Fluent;
-using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace FluentDemo
 {
     public class MainView : ContentPage
     {
-        private readonly Grid MainGrid = new Grid();
-        private readonly StackLayout MainStack = new StackLayout();
-        private readonly Label Label = new Label();
-
         public MainView()
         {
-            InitView();
-        }
-
-        private void InitView()
-        {
-            Content = MainGrid.RowsAndColumns(2)
-                .AddContent(() => new List<View>
-            {
-                MainStack.GridParent(0,0,2)
-                    .Alignment(LayoutOptions.Center, LayoutOptions.Center)
-                    .AddContent(() => new List<View>
-                    {
-                        Label.TextAlignment(TextAlignment.Center, TextAlignment.Center)
-                            .Text("Hello Xamarin Fluent!!!")
-                            .TextColor(Color.Gray)
-                    })
-            });
+            Content = new Grid()
+                .AddContent(
+                    new StackLayout()
+                        .Alignment(LayoutOptions.Center, LayoutOptions.Center)
+                        .AddContent(
+                            new Label().TextAlignment(TextAlignment.Center, TextAlignment.Center)
+                                .Text("Hello Xamarin Fluent", Color.Gray)));
         }
     }
 }
