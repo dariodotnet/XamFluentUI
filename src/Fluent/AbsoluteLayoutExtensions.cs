e7ceccadf5b0ecdc76a496a6f76f3ddc7a41ccc4
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:49:21 +0200

    Added AbsoluteLayout extension

---
+++ src/Fluent/AbsoluteLayoutExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace Fluent
+{
+    public static class AbsoluteLayoutExtensions
+    {
+        public static AbsoluteLayout Children(this AbsoluteLayout layout, Func<AbsoluteLayout.IAbsoluteList<View>> views)
+        {
+            views.Invoke().ForEach(x => layout.Children.Add(x));
+            return layout;
+        }
+    }
+}
