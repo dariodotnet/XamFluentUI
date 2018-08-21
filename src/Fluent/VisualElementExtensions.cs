6101b7b6886ab36931a035faf793f8279cf26f01
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:55:57 +0200

    Added extensions

--- src/Fluent/VisualElementExtensions.cs
+++ src/Fluent/VisualElementExtensions.cs
@@ -7,6 +7,13 @@ namespace Fluent
 {
     public static class VisualElementExtensions
     {
+        public static T Anchor<T>(this T visual, double anchorX, double anchorY) where T : VisualElement
+        {
+            visual.AnchorX = anchorX;
+            visual.AnchorY = anchorY;
+            return visual;
+        }
+
         public static T BackgroundColor<T>(this T visual, Color color) where T : VisualElement
         {
             visual.BackgroundColor = color;
@@ -16,6 +23,18 @@ namespace Fluent
         public static T Behaviors<T>(this T visual, Func<IList<Behavior>> behaviors) where T : VisualElement
         {
             behaviors.Invoke().ForEach(x => visual.Behaviors.Add(x));
+            return visual;
+        }
+
+        public static T Behaviors<T>(this T visual, params Behavior[] behaviors) where T : VisualElement
+        {
+            behaviors.ForEach(x => visual.Behaviors.Add(x));
+            return visual;
+        }
+
+        public static T FlowDirection<T>(this T visual, FlowDirection direction) where T : VisualElement
+        {
+            visual.FlowDirection = direction;
             return visual;
         }
 
@@ -45,9 +64,33 @@ namespace Fluent
             return visual;
         }
 
+        public static T InputTransparent<T>(this T visual, bool transparent) where T : VisualElement
+        {
+            visual.InputTransparent = transparent;
+            return visual;
+        }
+
+        public static T IsEnabled<T>(this T visual, bool enabled) where T : VisualElement
+        {
+            visual.IsEnabled = enabled;
+            return visual;
+        }
+
+        public static T IsVisible<T>(this T visual, bool visible) where T : VisualElement
+        {
+            visual.IsVisible = visible;
+            return visual;
+        }
+
         public static T Opacity<T>(this T visual, double opacity) where T : VisualElement
         {
             visual.Opacity = opacity;
+            return visual;
+        }
+
+        public static T Resources<T>(this T visual, ResourceDictionary dictionary) where T : VisualElement
+        {
+            visual.Resources = dictionary;
             return visual;
         }
 
@@ -59,9 +102,27 @@ namespace Fluent
             return visual;
         }
 
+        public static T Scale<T>(this T visual, double scale) where T : VisualElement
+        {
+            visual.Scale = scale;
+            return visual;
+        }
+
         public static T Style<T>(this T visual, Style style) where T : VisualElement
         {
             visual.Style = style;
+            return visual;
+        }
+
+        public static T StyleClass<T>(this T visual, Func<IList<string>> styleClass) where T : VisualElement
+        {
+            styleClass.Invoke().ForEach(x => visual.StyleClass.Add(x));
+            return visual;
+        }
+
+        public static T StyleClass<T>(this T visual, params string[] styleClass) where T : VisualElement
+        {
+            styleClass.ForEach(x => visual.StyleClass.Add(x));
             return visual;
         }
 
@@ -70,6 +131,18 @@ namespace Fluent
         {
             visual.TranslationX = translateX;
             visual.TranslationY = translateY;
+            return visual;
+        }
+
+        public static T Triggers<T>(this T visual, Func<IList<TriggerBase>> triggers) where T : VisualElement
+        {
+            triggers.Invoke().ForEach(x => visual.Triggers.Add(x));
+            return visual;
+        }
+
+        public static T Triggers<T>(this T visual, params TriggerBase[] triggers) where T : VisualElement
+        {
+            triggers.ForEach(x => visual.Triggers.Add(x));
             return visual;
         }
     }
