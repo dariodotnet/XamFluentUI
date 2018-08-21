a8c363e1ad915d4b811fc84d182d49534d4844b7
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:51:48 +0200

    Created ContentPage extensions

---
+++ src/Fluent/ContentPageExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class ContentPageExtensions
+    {
+        public static ContentPage ControlTemplate(this ContentPage page, ControlTemplate template)
+        {
+            page.ControlTemplate = template;
+            return page;
+        }
+    }
+}
