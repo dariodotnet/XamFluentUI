00dfd1b6abfd2c292cdeb099ed1071dc7b5e23a8
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:55:41 +0200

    Created TemplatedView extensions

---
+++ src/Fluent/TemplatedViewExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class TemplatedViewExtensions
+    {
+        public static TemplatedView ControlTemplate(this TemplatedView view, ControlTemplate template)
+        {
+            view.ControlTemplate = template;
+            return view;
+        }
+    }
+}
