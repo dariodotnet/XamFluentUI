7ee3acdab757810c1d8b5621adb552ab967f724d
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:52:22 +0200

    Created ContentView extensions

---
+++ src/Fluent/ContentViewExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class ContentViewExtensions
+    {
+        public static ContentView Content(this ContentView view, Func<View> content)
+        {
+            view.Content = content.Invoke();
+            return view;
+        }
+        
+        public static ContentView Content(this ContentView view, View content)
+        {
+            view.Content = content;
+            return view;
+        }
+    }
+}
