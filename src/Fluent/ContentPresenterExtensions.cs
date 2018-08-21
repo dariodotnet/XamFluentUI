fb5c020a654ffd907aa74846cc80b9c1522bdf45
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:52:07 +0200

    Created ContentPresenter extensions

---
+++ src/Fluent/ContentPresenterExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class ContentPresenterExtensions
+    {
+        public static ContentPresenter Content(this ContentPresenter presenter, Func<View> content)
+        {
+            presenter.Content = content.Invoke();
+            return presenter;
+        }
+        
+        public static ContentPresenter Content(this ContentPresenter presenter, View content)
+        {
+            presenter.Content = content;
+            return presenter;
+        }
+    }
+}
