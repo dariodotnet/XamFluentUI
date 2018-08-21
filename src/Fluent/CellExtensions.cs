6fa36533b9a975de6dca5787e76102d3a57aca41
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:51:33 +0200

    Created file for EntryCellExtensions

--- src/Fluent/CellExtensions.cs
+++ src/Fluent/CellExtensions.cs
@@ -16,28 +16,4 @@ namespace Fluent
             return cell;
         }
     }
-
-    public static class EntryCellExtensions
-    {
-        public static EntryCell HorizontalTextAlignment(this EntryCell cell, TextAlignment alignment)
-        {
-            cell.HorizontalTextAlignment = alignment;
-            return cell;
-        }
-        
-        public static EntryCell Keyboard(this EntryCell cell, Keyboard keyboard)
-        {
-            cell.Keyboard = keyboard;
-            return cell;
-        }
-        
-        public static EntryCell Text(this EntryCell cell, string text, string label = "", string placeholder = "", Color color = default(Color))
-        {
-            cell.Label = label;
-            cell.Placeholder = placeholder;
-            cell.LabelColor = color;
-            cell.Text = text;
-            return cell;
-        }
-    }
 }
---
+++ src/Fluent/EntryCellExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class EntryCellExtensions
+    {
+        public static EntryCell HorizontalTextAlignment(this EntryCell cell, TextAlignment alignment)
+        {
+            cell.HorizontalTextAlignment = alignment;
+            return cell;
+        }
+        
+        public static EntryCell Keyboard(this EntryCell cell, Keyboard keyboard)
+        {
+            cell.Keyboard = keyboard;
+            return cell;
+        }
+        
+        public static EntryCell Text(this EntryCell cell, string text, string label = "", string placeholder = "", Color color = default(Color))
+        {
+            cell.Label = label;
+            cell.Placeholder = placeholder;
+            cell.LabelColor = color;
+            cell.Text = text;
+            return cell;
+        }
+    }
+}
