c8eac6728cdb5c721d97c136b746c6d56c861e12
Darío Alonso <dariovillar@hotmail.com>
2018-08-21 19:52:35 +0200

    Created Editor extensions

---
+++ src/Fluent/EditorExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class EditorExtensions
+    {
+        public static Editor AutoSize(this Editor editor, EditorAutoSizeOption autoSize)
+        {
+            editor.AutoSize = autoSize;
+            return editor;
+        }
+        
+        public static Editor FontAttributes(this Editor editor, FontAttributes attributes)
+        {
+            editor.FontAttributes = attributes;
+            return editor;
+        }
+        
+        public static Editor FontFamily(this Editor editor, string fontFamilty)
+        {
+            editor.FontFamily = fontFamilty;
+            return editor;
+        }
+        
+        public static Editor FontSize(this Editor editor, double size)
+        {
+            editor.FontSize = size;
+            return editor;
+        }
+        
+        public static Editor Text(this Editor editor, string text, Color color = default(Color))
+        {
+            editor.Text = text;
+            editor.TextColor = color;
+            return editor;
+        }
+    }
+}
