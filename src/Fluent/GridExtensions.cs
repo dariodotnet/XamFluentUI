6501f08aff2fe36448c1531d96f12fec29112874
Darío Alonso <dariovillar@hotmail.com>
2018-08-22 17:10:56 +0200

    Added configuration options

--- src/Fluent/GridExtensions.cs
+++ src/Fluent/GridExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace Fluent
 {
@@ -7,46 +9,33 @@ namespace Fluent
         public static Grid RowsAndColumns(this Grid grid, int rows = 1, int columns = 1)
         {
             if (rows > 1)
-            {
-                grid.RowDefinitions = new RowDefinitionCollection();
-                for (var i = 0; i < rows; i++)
-                {
-                    grid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
-                }
-            }
+                grid = AddRows(grid, rows);
 
             if (columns > 1)
-            {
-                grid.ColumnDefinitions = new ColumnDefinitionCollection();
-                for (var i = 0; i < rows; i++)
-                {
-                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Star });
-                }
-            }
+                grid = AddColumns(grid, columns);
+                
             return grid;
         }
 
         public static Grid AddRows(this Grid grid, int rows = 0)
         {
-            if (rows > 0)
-            {
-                for (var i = 0; i < rows; i++)
-                {
-                    grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
-                }
-            }
+            if (rows < 1) 
+                return grid;
+            
+            for (var i = 0; i < rows; i++)
+                grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            
             return grid;
         }
 
         public static Grid AddColumns(this Grid grid, int columns = 0)
         {
-            if (columns > 0)
-            {
-                for (var i = 0; i < columns; i++)
-                {
-                    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
-                }
-            }
+            if (columns < 1) 
+                return grid;
+            
+            for (var i = 0; i < columns; i++)
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+            
             return grid;
         }
 
@@ -56,9 +45,21 @@ namespace Fluent
             return grid;
         }
 
+        public static Grid ConfigureColumns(this Grid grid, params Tuple<int, GridLength>[] values)
+        {
+            values.ForEach(x => grid.ColumnDefinitions[x.Item1].Width = x.Item2);
+            return grid;
+        }
+
         public static Grid ConfigureRow(this Grid grid, int row, GridLength length)
         {
             grid.RowDefinitions[row].Height = length;
+            return grid;
+        }
+
+        public static Grid ConfigureRows(this Grid grid, params Tuple<int, GridLength>[] values)
+        {
+            values.ForEach(x => grid.RowDefinitions[x.Item1].Height = x.Item2);
             return grid;
         }
 
