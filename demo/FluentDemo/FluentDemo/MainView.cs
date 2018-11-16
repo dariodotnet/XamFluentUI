b2d003f0433fdc2d29c746b54b8309ecc943c578
Darío Alonso <dariovillar@hotmail.com>
2018-11-16 13:01:58 +0100

    using events extensions

--- demo/FluentDemo/FluentDemo/MainView.cs
+++ demo/FluentDemo/FluentDemo/MainView.cs
@@ -1,5 +1,4 @@
 ﻿using Fluent;
-using System;
 using Xamarin.Forms;
 
 namespace FluentDemo
@@ -7,6 +6,7 @@ namespace FluentDemo
     public class MainView : ContentPage
     {
         private readonly Button NavigateTo = new Button();
+
         public MainView()
         {
             Content = new Grid()
@@ -14,18 +14,17 @@ namespace FluentDemo
                     new StackLayout()
                         .Alignment(LayoutOptions.Center, LayoutOptions.Center)
                         .AddContent(
+
                             new Label().TextAlignment(TextAlignment.Center, TextAlignment.Center)
                                 .Text("Hello Xamarin Fluent", Color.Gray),
-                            NavigateTo.BackgroundColor(Color.CornflowerBlue)
+
+                            NavigateTo.ClickedEvent(async () =>
+                                {
+                                    await Application.Current.MainPage.Navigation.PushAsync(new PageTwo());
+                                })
+                                .BackgroundColor(Color.CornflowerBlue)
                                 .Text("Go to page two", Color.DarkSlateGray))
                    );
-
-            NavigateTo.Clicked += NavigateToOnClicked;
-        }
-
-        private void NavigateToOnClicked(object sender, EventArgs e)
-        {
-            Application.Current.MainPage.Navigation.PushAsync(new PageTwo());
         }
     }
 }
--- demo/FluentDemo/FluentDemo/PageTwo.cs
+++ demo/FluentDemo/FluentDemo/PageTwo.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
-using Fluent;
+﻿using Fluent;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace FluentDemo
 {
     public class PageTwo : ContentPage
     {
+        private Button ClickMe = new Button();
+        private TapGestureRecognizer TapBack = new TapGestureRecognizer();
+
         public PageTwo()
         {
             Content = new Grid()
@@ -14,12 +17,25 @@ namespace FluentDemo
                 {
                     new StackLayout().Alignment(LayoutOptions.Center, LayoutOptions.Center)
                         .AddContent(
-                            new Entry().Placeholder("Placeholder text", Color.DarkSlateGray), 
-                            new Button().BackgroundColor(Color.LightSkyBlue)
+                            new Entry().Placeholder("Placeholder text", Color.DarkSlateGray),
+                            ClickMe.BackgroundColor(Color.LightSkyBlue)
                                 .Text("ClickMe", Color.Black)
                                 .CornerRadius(5)
-                                .FontAttributes(FontAttributes.Bold))
+                                .FontAttributes(FontAttributes.Bold),
+                            new Grid().BackgroundColor(Color.LightCoral)
+                                .Alignment(LayoutOptions.StartAndExpand)
+                                .SizeRequest(ClickMe.Width, ClickMe.Height)
+                                .AddContent(() => new List<View>
+                                {
+                                    new Label().Text("Go back")
+                                })
+                                .GestureRecognizers(() => new[] { TapBack.TappedEvent(GoBack) }))
                 });
+        }
+
+        private async void GoBack()
+        {
+            await Application.Current.MainPage.Navigation.PopAsync(true);
         }
     }
 }
