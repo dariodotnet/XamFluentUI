2104f4bb490bd5154804668817676964a91c55ad
Darío Alonso <dariovillar@hotmail.com>
2019-01-25 20:33:37 +0100

    TemplatedView to generic

--- src/Fluent/TemplatedViewExtensions.cs
+++ src/Fluent/TemplatedViewExtensions.cs
@@ -4,7 +4,7 @@ namespace Fluent
 {
     public static class TemplatedViewExtensions
     {
-        public static TemplatedView ControlTemplate(this TemplatedView view, ControlTemplate template)
+        public static T ControlTemplate<T>(this T view, ControlTemplate template) where T : TemplatedView
         {
             view.ControlTemplate = template;
             return view;
