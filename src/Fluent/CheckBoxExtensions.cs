e5995e1e9242269af1ab0612691d233559402c08
Darío Alonso <dariovillar@hotmail.com>
2019-06-29 11:44:00 +0200

    Create extensions for check box

---
+++ src/Fluent/CheckBoxExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Fluent
+{
+    using Xamarin.Forms;
+
+    public static class CheckBoxExtensions
+    {
+        public static CheckBox Color(this CheckBox checkBox, Color color)
+        {
+            checkBox.Color = color;
+            return checkBox;
+        }
+
+        public static CheckBox IsChecked(this CheckBox checkBox, bool isChecked)
+        {
+            checkBox.IsChecked = isChecked;
+            return checkBox;
+        }
+    }
+}
