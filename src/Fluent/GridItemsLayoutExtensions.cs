b6758e9f9f9505cc69a682797191e5027b3e96b0
Darío Alonso <dariovillar@hotmail.com>
2019-07-02 21:59:03 +0200

    GridLayout and ListItemsLayouts extensions

--- src/Fluent/GridItemsLayoutExtensions.cs
+++ src/Fluent/GridItemsLayoutExtensions.cs
@@ -9,5 +9,17 @@ namespace Fluent
             element.Span = span;
             return element;
         }
+
+        public static GridItemsLayout HorizontalItemSpacing(this GridItemsLayout grid, double spacing)
+        {
+            grid.HorizontalItemSpacing = spacing;
+            return grid;
+        }
+
+        public static GridItemsLayout VerticalItemSpacing(this GridItemsLayout grid, double spacing)
+        {
+            grid.VerticalItemSpacing = spacing;
+            return grid;
+        }
     }
 }
---
+++ src/Fluent/ListItemsLayoutExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Fluent
+{
+    public static class ListItemsLayoutExtensions
+    {
+        public static ListItemsLayout ItemsSpacing(this ListItemsLayout layout, double spacing)
+        {
+            layout.ItemSpacing = spacing;
+            return layout;
+        }
+    }
+}
