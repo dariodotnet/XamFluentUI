dc9f658049316263a67c4bbd2f331c2cd30c5110
Darío Alonso <dariovillar@hotmail.com>
2020-02-11 19:36:19 +0000

    Update Xamarin.Forms

--- demo/FluentDemo/FluentDemo.Android/MainActivity.cs
+++ demo/FluentDemo/FluentDemo.Android/MainActivity.cs
@@ -1,14 +1,11 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace FluentDemo.Droid
 {
+
+
     [Activity(Label = "FluentDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
