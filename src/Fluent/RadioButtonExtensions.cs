2f34f11796220fda9d07bb2e6f27584841cca45d
Darío Alonso <dariovillar@hotmail.com>
2020-03-05 18:40:44 +0000

    Update extensions

---
+++ src/Fluent/RadioButtonExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Fluent
+{
+    using Xamarin.Forms;
+
+    public static class RadioButtonExtensions
+    {
+        public static RadioButton ButtonSource(this RadioButton button, ImageSource source)
+        {
+            button.ButtonSource = source;
+            return button;
+        }
+
+        public static RadioButton GroupName(this RadioButton button, string group)
+        {
+            button.GroupName = group;
+            return button;
+        }
+
+        public static RadioButton Config(this RadioButton button, ImageSource source, string group)
+        {
+            button.ButtonSource = source;
+            button.GroupName = group;
+            return button;
+        }
+
+        public static RadioButton IsChecked(this RadioButton button, bool isChecked)
+        {
+            button.IsChecked = isChecked;
+            return button;
+        }
+    }
+}
--- src/Fluent/WebViewExtensions.cs
+++ src/Fluent/WebViewExtensions.cs
@@ -2,11 +2,27 @@
 
 namespace Fluent
 {
+    using System.Net;
+
     public static class WebViewExtensions
     {
         public static WebView Source(this WebView webView, WebViewSource source)
         {
             webView.Source = source;
+            return webView;
+        }
+
+        public static WebView Cookies(this WebView webView, CookieContainer container)
+        {
+            webView.Cookies = container;
+            return webView;
+        }
+
+        public static WebView Config(this WebView webView, WebViewSource source, CookieContainer container)
+        {
+            webView.Source = source;
+            webView.Cookies = container;
+
             return webView;
         }
     }
